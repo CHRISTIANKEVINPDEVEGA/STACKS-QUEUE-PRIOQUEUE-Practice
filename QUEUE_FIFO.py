267aa57b587cf919f7625e03f9ef9b004359e9cc
unknown <Christiandevega12@gmail.com>
2022-12-11 13:59:49 +0800

    added iteration and length reader in class

--- QUEUE_FIFO.py
+++ QUEUE_FIFO.py
@@ -1,8 +1,15 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+    
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
 
     def enqueue(self,element):
         self._elements.append(element)
@@ -10,11 +17,5 @@ class Queue:
     def dequeue(self):
         return self._elements.popleft()
 
-fifo=Queue()
-fifo.enqueue("una sa linya")
-fifo.enqueue("pangalwa sa linya")
-fifo.enqueue("pangtalo sa linya")
-
-print(fifo.dequeue())
-print(fifo.dequeue())
-print(fifo.dequeue())
+fifo=Queue("Una","Pangalwa","Pangatlo")
+print(len(fifo))
