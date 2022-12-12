21396ff68b3da45498c4823adaaeb03f2401910c
unknown <Christiandevega12@gmail.com>
2022-12-12 14:03:17 +0800

    added a list the simulate a manual stack lifo

--- LIFO_tester.py
+++ LIFO_tester.py
@@ -3,3 +3,13 @@ from QUEUE import Stack
 inventory_lifo=Stack("una","pangalwa","pangatlo")
 for elements in inventory_lifo:
     print(elements)
+
+
+inventory_lifo_=[]
+inventory_lifo_.append("first")
+inventory_lifo_.append("second")
+inventory_lifo_.append("third")
+
+print(f"\n{inventory_lifo_.pop()}")
+print(inventory_lifo_.pop())
+print(inventory_lifo_.pop())
