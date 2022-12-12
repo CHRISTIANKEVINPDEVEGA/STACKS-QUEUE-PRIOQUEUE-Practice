8bdde2e1064c6dca72897e69bfd96354507b1377
unknown <Christiandevega12@gmail.com>
2022-12-12 15:03:14 +0800

    added tester for heappop() and how the program reorganize the elements inside the list

--- HEAP_tester.py
+++ HEAP_tester.py
@@ -1,8 +1,17 @@
 from heapq import heappush
+from heapq import heappop
 
 fruits=[]
 heappush(fruits,"orange")
 heappush(fruits,"apple")
 heappush(fruits,"banana")
+
+print(fruits)
+
+print(f"\n")
+
+print(heappop(fruits))
+
+print(f"\n")
 
 print(fruits)
