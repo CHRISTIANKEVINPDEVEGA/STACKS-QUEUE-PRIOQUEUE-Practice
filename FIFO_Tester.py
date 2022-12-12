0a0f801f203cbc337cc71cf0fba91873ccf8b3b5
unknown <Christiandevega12@gmail.com>
2022-12-12 13:27:25 +0800

    all tested QUEUE class will be in separate .py file

---
+++ FIFO_Tester.py
@@ -0,0 +1,10 @@
+
+from QUEUE import Queue
+
+inventory_fifo=Queue("Una","Pangalwa","Pangatlo")
+print(len(inventory_fifo))
+
+for element in inventory_fifo:
+    print(element)
+
+print(len(inventory_fifo))
