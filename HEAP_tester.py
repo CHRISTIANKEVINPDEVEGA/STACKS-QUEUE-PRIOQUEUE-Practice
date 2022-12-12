47831feb3ba9f3ed380846e22873ef0dbe872222
unknown <Christiandevega12@gmail.com>
2022-12-12 15:24:51 +0800

    added a tester for tuple behavior when comparing the elements

--- HEAP_tester.py
+++ HEAP_tester.py
@@ -15,3 +15,12 @@ print(heappop(fruits))
 print(f"\n")
 
 print(fruits)
+
+print(f"\n")
+
+person1= ("Bern" , "Black", 35 )
+person2= ("Bern" , "Tehn", 35 )
+person3= ("Bern" , "Tehn", 12 )
+
+print(person1 < person2)
+print(person2 < person3)
