722f69c6e7f911ed206dab6d28a14ec050ff50a2
unknown <Christiandevega12@gmail.com>
2022-12-13 17:04:33 +0800

    added for loop for printing the nodes connected to the specified node

--- GRAPH_tester.py
+++ GRAPH_tester.py
@@ -4,6 +4,9 @@ from GRAPH import City_name
 
 nodes, graph = graph_load('roadmap.dot', City_name.from_dict)
 
-print(nodes["london"])
+print(nodes["york"])
 
 print(graph)
+
+for neighbor in graph.neighbors(nodes["york"]):
+    print(neighbor.name)
