3573c7b50c0d49712a78b3184186a500579d9166
unknown <Christiandevega12@gmail.com>
2022-12-13 19:50:44 +0800

    added a function that make the program read neighbors with higher latitude first

--- BREADTH-FIRST-FIFOQ.py
+++ BREADTH-FIRST-FIFOQ.py
@@ -4,9 +4,14 @@ from GRAPH import City_name, graph_load
 def in_twentieth_century(year):
     return year and 1901 <= year <=2000
 
+def arrangement(neighbors):
+    def use_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=use_latitude, reverse=True))     
+
 nodes, graph = graph_load("roadmap.dot", City_name.from_dict)
 
-for _node in nx.bfs_tree(graph, nodes["york"]):
+for _node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=arrangement):
     print("@", _node.name)
     if in_twentieth_century(_node.year):
         print("Found: ", _node.name, _node.year)
