0b304c17c12d5fe71f5af041a68c21aaf6fcdbf3
unknown <Christiandevega12@gmail.com>
2022-12-13 17:48:53 +0800

    fixed the bug cause by wrong function name in def sorting()

--- GRAPH_tester.py
+++ GRAPH_tester.py
@@ -14,11 +14,14 @@ for neighbor in graph.neighbors(nodes["york"]):
 for neighbor, node_weights in graph[nodes["york"]].items():
     print(node_weights["distance"],neighbor.name)
 
+print("\n")
+
 def sorting(neighbors, approach):
-    return sorting(neighbors.items(), key=lambda item: approach(item[1]))
+    return sorted(neighbors.items(), key = lambda item: approach(item[1]))
 
 def distance_length(node_weights):
     return float(node_weights["distance"])
 
-for neighbors, node_weights in sorting(graph[nodes["york"]],distance_length):
-    print(f"{node_weights['distance']:>3} miles, {neighbors.name}")
+for neighbor, node_weights in sorting(graph[nodes["york"]], distance_length):
+    print(f"{node_weights['distance']:>3} miles, {neighbor.name}")
+
