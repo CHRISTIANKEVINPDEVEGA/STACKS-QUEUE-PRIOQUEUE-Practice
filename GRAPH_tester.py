b82787290aeddd1da596ab2d788560ae1f4770d0
unknown <Christiandevega12@gmail.com>
2022-12-13 17:36:25 +0800

    added sorter for the distance and neighbor city

--- GRAPH_tester.py
+++ GRAPH_tester.py
@@ -13,3 +13,12 @@ for neighbor in graph.neighbors(nodes["york"]):
 
 for neighbor, node_weights in graph[nodes["york"]].items():
     print(node_weights["distance"],neighbor.name)
+
+def sorting(neighbors, approach):
+    return sorting(neighbors.items(), key=lambda item: approach(item[1]))
+
+def distance_length(node_weights):
+    return float(node_weights["distance"])
+
+for neighbors, node_weights in sorting(graph[nodes["york"]],distance_length):
+    print(f"{node_weights['distance']:>3} miles, {neighbors.name}")
