124d934d311eb5164fca4533d77c9674c05951e6
unknown <Christiandevega12@gmail.com>
2022-12-14 20:20:07 +0800

    added disjkstra algorithm on the shortest path program

--- GRAPH.py
+++ GRAPH.py
@@ -2,6 +2,8 @@ from typing import NamedTuple
 import networkx as nx
 from QUEUE import Queue, Stack
 from collections import deque
+from math import inf as infinity
+from QUEUE import MutableMinHeap, Queue, Stack
 
 
 class City_name(NamedTuple):
@@ -120,3 +122,24 @@ def search(traverse, graph, source, predicate, order_by=None):
     for node in traverse(graph, source, order_by):
         if predicate(node):
             return node
+
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
+    while unvisited:
+        visited.add(node := unvisited.dequeue())
+        for neighbor, weights in graph[node].items():
+            if neighbor not in visited:
+                weight = weight_factory(weights)
+                new_distance = unvisited[node] + weight
+                if new_distance < unvisited[neighbor]:
+                    unvisited[neighbor] = new_distance
+                    previous[neighbor] = node
+
+    return retrace(previous, source, destination)
