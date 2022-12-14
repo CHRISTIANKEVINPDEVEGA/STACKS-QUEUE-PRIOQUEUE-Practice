e5bbc69e75341fc8e3fba8c00a5e5554a980da6c
unknown <Christiandevega12@gmail.com>
2022-12-14 19:07:26 +0800

    added a tester for the retrace and shortest_path function

--- SHORT-BFFQ.py
+++ SHORT-BFFQ.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from GRAPH import City_name, graph_load
+from GRAPH import City_name, graph_load, shortest_path
 
 nodes, graph = graph_load("roadmap.dot", City_name.from_dict)
 
@@ -8,3 +8,18 @@ city2=nodes["perth"]
 
 for any, path in enumerate(nx.all_shortest_paths(graph,city1,city2),1):
     print(f"{any}.","->".join(city.name for city in path))
+
+print("\n\n\n")
+
+
+
+print(" -> ".join(city.name for city in shortest_path(graph, city1, city2)))
+
+
+def by_latitude(city):
+    return -city.latitude
+
+print(" -> ".join(
+    city.name
+    for city in shortest_path(graph, city1, city2, by_latitude)
+))
