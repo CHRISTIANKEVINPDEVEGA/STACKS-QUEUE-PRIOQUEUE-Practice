ade4622b554c304e6bca75844b156d13259bbee5
unknown <Christiandevega12@gmail.com>
2022-12-16 15:07:29 +0800

    added main function for web crawler program

--- AsynchronousQueues/A_QUEUE.py
+++ AsynchronousQueues/A_QUEUE.py
@@ -2,3 +2,11 @@ import argparse
 import asyncio
 from collections import Countergit 
 import aiohttp
+
+async def main(args):
+    session = aiohttp.ClientSession()
+    try:
+        links = Counter()
+        display(links)
+    finally:
+        await session.close()
