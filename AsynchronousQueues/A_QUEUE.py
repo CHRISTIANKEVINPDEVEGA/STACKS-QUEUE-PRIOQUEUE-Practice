3e45efa4dcd6af8b6c673d690bfe08a4e47d21a2
unknown <Christiandevega12@gmail.com>
2022-12-16 15:33:56 +0800

    added a condition that will run the program

--- AsynchronousQueues/A_QUEUE.py
+++ AsynchronousQueues/A_QUEUE.py
@@ -1,10 +1,10 @@
 import argparse
 import asyncio
-from collections import Countergit 
+from collections import Counter
 import aiohttp
 
 async def main(args):
-    session = aiohttp.ClientSession()
+    session = aiohttp.ClientSession()   
     try:
         links = Counter()
         display(links)
@@ -21,3 +21,6 @@ def parse_args():
 def display(links):
     for url, count in links.most_common():
         print(f"{count:>3} {url}")
+
+if __name__ == "__main__":
+    asyncio.run(main(parse_args()))   
